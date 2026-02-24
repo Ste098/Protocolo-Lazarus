40738a9dc512197f642e99f3fd87fb9ecdbed0d4
Ste098 <idk.321341@gmail.com>
2026-02-23 19:29:15 -0500

    Add files via upload

--- protocolo--lazarus/Prueba Esguerra/personaje/esguerra.gd
+++ protocolo--lazarus/Prueba Esguerra/personaje/esguerra.gd
@@ -4,21 +4,29 @@ extends CharacterBody2D
 @export var gravity = 100
 @export var jump_speed = 100
 @export var speed = 100
+@export var bullet_scene: PackedScene
 
 func _physics_process(delta):
-	
-	
-	var direction = Input.get_axis("izquierda", "derecha")
-	velocity.x = speed * direction
-	
+    
+    
+    var direction = Input.get_axis("izquierda", "derecha")
+    velocity.x = speed * direction
+    
 #gravedad
-	if not is_on_floor():
-		velocity.y = velocity.y + gravity  * delta 
-	
-	#salto
-	var jump_pressed = Input.is_action_just_pressed("saltar")
-	if jump_pressed: 
-		velocity.y = velocity.y - jump_speed
-	
-	
-	move_and_slide()
+    if not is_on_floor():
+        velocity.y = velocity.y + gravity  * delta 
+    
+    #salto
+    var jump_pressed = Input.is_action_just_pressed("saltar")
+    if jump_pressed: 
+        velocity.y = velocity.y - jump_speed
+    
+    if Input.is_action_just_pressed("disparar"):
+        shoot()
+    
+    move_and_slide()
+    
+func shoot():
+        var bullet = bullet_scene.instantiate()
+        bullet.position = global_position
+        get_parent().add_child(bullet)
---
+++ protocolo--lazarus/bala.gd
@@ -0,0 +1,7 @@
+extends Area2D
+
+@export var speed = 400
+var direction = 1
+
+func _physics_process(delta):
+	position.x += speed * direction * delta
